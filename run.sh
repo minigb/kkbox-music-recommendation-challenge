80713961f91cf90320ff4d0b388657cfa74f0495
Minhee Lee <minigimbob@gmail.com>
2024-12-04 14:43:49 +0900

    ss

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Run main_lgbm.py
-python3 main_lgbm.py
+# List of Python files to run
+python_files=("main_lgbm.py" "main_predict.py")
 
-# Check if the previous command was successful
-if [ $? -eq 0 ]; then
-    # Run main_predict.py
-    python3 main_predict.py
-else
-    echo "main_lgbm.py failed. Exiting."
-    exit 1
-fi
+# Loop through the list and run each file
+for file in "${python_files[@]}"; do
+    python3 "$file"
+    if [ $? -ne 0 ]; then
+        echo "$file failed. Exiting."
+        exit 1
+    fi
+done
