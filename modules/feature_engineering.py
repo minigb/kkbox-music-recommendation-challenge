eb49ad5ef6dbeae9d7b0fa3e39d5f2f05b8e5d6b
Minhee Lee <minigimbob@gmail.com>
2024-12-06 17:18:49 +0900

    ss

--- modules/feature_engineering.py
+++ modules/feature_engineering.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
 class FeatureEngineering:
-    def __init__(self, data):
+    def __init__(self, data, config):
         self.data = data # interaction data
+        self.config = config
     
     def refine_composer(self):
         composers_list = [sorted(str(composers).split('| ')) for composers in self.data['composer'].tolist()]
         # max_composers = max([len(composers) for composers in composers_list])
-        max_composers = 10
+        max_composers = self.config.feature_engineering.max_composers
         for i in range(max_composers):
             self.data[f'composer_{i}'] = [composers[i] if len(composers) > i else None for composers in composers_list]
         self.data.drop(columns=['composer'], inplace=True)
