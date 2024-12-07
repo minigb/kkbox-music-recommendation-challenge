278ff4488c170d1793cf869d012d81996fd884a5
Minhee Lee <minigimbob@gmail.com>
2024-12-07 13:46:13 +0900

    Create parent directory before saving files

--- utils/pickle_util.py
+++ utils/pickle_util.py
@@ -1,9 +1,11 @@
 import pickle
+from pathlib import Path
 
 def load_pkl(file_path):
     with open(file_path, 'rb') as f:
         return pickle.load(f)
     
 def save_pkl(obj, file_path):
+    Path(file_path).parent.mkdir(parents=True, exist_ok=True)
     with open(file_path, 'wb') as f:
         pickle.dump(obj, f)
